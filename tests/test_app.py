c6f618212456f4d24a49e1db732e71e5398e96f6
Rupert Angermeier <rupert.angermeier@gmail.com>
2013-03-17 13:09:42 +0100

    add basic tests for index, system and disks pages

---
+++ tests/test_app.py
@@ -0,0 +1,32 @@
+import sys, os
+# append parent directory to PYTHONPATH so imports work
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+import flaskberry
+import unittest
+from nose.tools import assert_equal, assert_true, assert_in
+
+class FlaskberryTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = flaskberry.app.test_client()
+
+    def test_index(self):
+        rv = self.app.get('/')
+        assert_in("Welcome", rv.data)
+        assert_equal(rv.status, '200 OK')
+
+    def test_system_page(self):
+        rv = self.app.get('/system/')
+        assert_in("System", rv.data)
+        assert_in("load average", rv.data)
+        assert_in("shutdown", rv.data)
+        assert_in("reboot", rv.data)
+
+    # testing disk module requires sudo
+    def test_disks_page(self):
+        rv = self.app.get('/disks/')
+        assert_in("Disks", rv.data)
+        assert_in("unmount", rv.data)
+
+if __name__ == '__main__':
+    unittest.main()
